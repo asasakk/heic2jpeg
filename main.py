33d75ef2195d92a3e66bf7a9339cd8d549ef3fc4
asasakk <53592756+asasakk@users.noreply.github.com>
2024-05-17 15:02:46 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import pillow_heif
+import glob
+import pathlib
+
+dir = r"C:\Users\ko\Pictures\img"
+
+def heic_jpg(image_path, save_path):
+    try:
+        heif_file = pillow_heif.read_heif(image_path)
+        for img in heif_file: 
+            image = Image.frombytes(
+                img.mode,
+                img.size,
+                img.data,
+                'raw',
+                img.mode,
+                img.stride,
+            )
+        image.save(save_path, "JPEG")
+    except Exception as e:
+        print(f"Error processing {image_path}: {e}")
+
+image_dir = pathlib.Path(dir)
+heic_path = list(image_dir.glob('**/*.HEIC'))
+
+for i in heic_path:
+    image_path = str(i)
+    save_path = str(image_dir / i.stem) + '.jpg'
+    print(save_path)
+    heic_jpg(image_path, save_path)
